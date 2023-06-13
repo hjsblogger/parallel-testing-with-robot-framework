505b5c8683798576d5508c4f95c13dc000927f5f
Himanshu Sheth <himanshu.sheth@gmail.com>
2023-06-14 00:42:08 +0530

    Update Locators.py

--- Resources/PageObject/Locators/Locators.py
+++ Resources/PageObject/Locators/Locators.py
@@ -1,4 +1,5 @@
 # ToDo Page Locators
+
 FirstItem = "name:li1"
 SecondItem = "name:li2"
 FifthItem = "name:li5"
@@ -9,6 +10,7 @@ NewItemText = "Yey Let's add it to list"
 NewAdditionText = "xpath=/html/body/div/div/div/ul/li[6]/span"
 
 # Selenium Playground Page Locators
+
 SubmitButton = "xpath=//a[.='Input Form Submit']"
 Name = "name:name"
 email = "id:inputEmail4"
@@ -21,4 +23,7 @@ address1 = "id:inputAddress1"
 address2 = "id:inputAddress2"
 state = "css=#inputState"
 city = "//input[@id='inputCity']"
-address1 = "css=#inputZip"
+zipcode = "css=#inputZip"
+FinalSubmission = "xpath=//*[@id='seleniumform']/div[6]/button"
+
+SuccessText = "Thanks for contacting us, we will get back to you shortly."
